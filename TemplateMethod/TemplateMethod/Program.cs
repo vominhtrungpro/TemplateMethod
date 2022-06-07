5f035bd6fd327c3ffcec19de49a3c347a9abcb7b
vominhtrungpro <68464213+vominhtrungpro@users.noreply.github.com>
2022-06-07 13:44:57 +0700

    Add files via upload

---
+++ TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Build build = new Build();
+        BuildOnStaging build = new BuildOnStaging();
+        Console.ReadKey();
+    }
+
+    class Build
+    {
+        public Build()
+        {
+            CompileCode();
+            RunTests();
+            DeployCode();
+        }
+
+        public virtual void CompileCode()
+        {
+            Console.WriteLine("Code is compile!");
+        }
+        public virtual void RunTests()
+        {
+            Console.WriteLine("Test ran successfully!");
+        }
+        public virtual void DeployCode()
+        {
+            Console.WriteLine("Deployed to QE server!");
+        }
+    }
+
+    class BuildOnStaging : Build
+    {
+        
+        public override void DeployCode()
+        {
+            Console.WriteLine("Deployed to Staging server!");
+        }
+    }
+
+}
